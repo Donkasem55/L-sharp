55f08f2f207a3c6d0e300af254c0bc167dd2a010
TheLuckyCuber999 <92718237+Donkasem55@users.noreply.github.com>
2026-04-05 18:30:13 +0800

    idk

--- main.py
+++ main.py
@@ -42,194 +42,202 @@ libs = {}
 bss = []
 scope = ["global"]
 fncs = []
+currentline = 0
 
-linecount = len(d)
-i = 0
-while i < linecount:
-    line = d[i]
+def codegen(data):
+    if isinstance(data[0], list):
+        print(f"SyntaxError: I don't even know how you got this error, but the AST returned an array as the first keyword. This was at line {currentline}")
+    pre, bss, out, = [], [], [], [], []
+    global vartype, links, mainfn, libs, scope, fncs, externs
+    linecount = len(data)
+    i = 0
+    while i < linecount:
+        line = data[i]
 
-    if line == [] or line[0].startswith("//"):
-        i += 1
-        continue
+        if line == [] or line[0].startswith("//"):
+            i += 1
+            continue
 
-    for j in range(len(line)):
-        if line[j] in ma:
-            line[j] = ma[line[j]]
+        for j in range(len(line)):
+            if line[j] in ma:
+                line[j] = ma[line[j]]
 
-    if line[0] == "include":
-        with open(f"{scr}/stdlib/{kernel}/{line[1]}") as f:
-            asm = f.read().splitlines()
-            include = asm[4:]
-            lnk = asm[0].split(" ")
-            lnk.pop(0)
+        if line[0] == "include":
+            with open(f"{scr}/stdlib/{kernel}/{line[1]}") as f:
+                asm = f.read().splitlines()
+                include = asm[4:]
+                lnk = asm[0].split(" ")
+                lnk.pop(0)
 
-            extern = asm[2].split(" ")
-            extern.pop(0)
+                extern = asm[2].split(" ")
+                extern.pop(0)
 
-            newlib = []
-            for j in include:
-                newlib.append(j)
+                newlib = []
+                for j in include:
+                    newlib.append(j)
 
-            libname = line[1].split(".")[0]
-            libs[libname] = newlib
+                libname = line[1].split(".")[0]
+                libs[libname] = newlib
 
-            for j in lnk:
-                links.append(j)
-            for j in extern:
-                externs.append(j)
+                for j in lnk:
+                    links.append(j)
+                for j in extern:
+                    externs.append(j)
 
-    elif line[0] == "entry":
-        mainfn = line[1]
+        elif line[0] == "entry":
+            mainfn = line[1]
 
-    elif line[0] == "varinit":
-        try:
-            if pre[0] != "default rel\nsection .data":
-                pre = ["default rel\nsection .data"] + pre
-        except IndexError:
-            pre = ["default rel\nsection .data"]
+        elif line[0] == "varinit":
+            try:
+                if pre[0] != "default rel\nsection .data":
+                    pre = ["default rel\nsection .data"] + pre
+            except IndexError:
+                pre = ["default rel\nsection .data"]
 
-        try:
-            if bss[0] != "section .bss":
-                bss = ["section .bss"] + bss
-        except IndexError:
-            bss = ["section .bss"]
-    elif line[0] == "byte":
-        try:
-            if line[2] == "=>":
-                pre.append(f"{line[1]} db {" ".join(line[3:])}")
-                vartype[line[1]] = "byte"
-            elif line[2] == "<=":
-                bss.append(f"{line[1]}: resb {line[3]}")
-                vartype[line[1]] = "byteptr"
-
-        except IndexError:
-            bss.append(f"{line[1]}: resb 1")
-            vartype[line[1]] = "byteptr"
-
-    elif line[0] == "pipe":
-        try:
-            if line[2] == "=>":
-                pre.append(f"{line[1]} EQU {' '.join(line[3:])}")
-            elif line[2] == "<=":
-                pre.append(f"{line[3]} EQU {line[1]}")
-        except IndexError:
-            print(f"Error: At line {i}")
-
-    elif line[0] == "short":
-        try:
-            if line[2] == "=>":
-                pre.append(f"{line[1]} dw {line[3]}")
-                vartype[line[1]] = "short"
-
-        except IndexError:
-            pre.append(f"{line[1]}: resb 2")
-            vartype[line[1]] = "shortptr"
-
-    elif line[0] == "dword":
-        if line[2] == "<=":
-            bss.append(f"{line[1]}: resd {line[3]}")
-
-
-    elif line[0] == "asmdef":
-        pre.append(" ".join(line[1:]))
-
-    elif line[0] == "asm":
-        out.append(" ".join(line[1:]))
-
-    elif line[0] == "label":
-        pre.append(f"{line[1]}:")
-
-    elif line[0] == "return":
-        try:
-            out.append(f"mov rax, {line[1]}")
-        except IndexError:
-            pass
-        out.append("ret")
-
-    elif line[0] == "=>":
-        out.append(f"jmp {line[1]}")
-
-    else:
-        try:
-            if line[1] == "<=" and line[0] not in libs and line[0] not in fncs:
-                out.append(f"mov {line[0]}, {line[2]}")
-                i += 1
-                continue
-
-            elif line[1] == "<":
-                match line[2]:
-                    case "+":
-                        out.append(f"add {line[0]}, {line[3]}")
-
-                    case "-":
-                        out.append(f"sub {line[0]}, {line[3]}")
-
-                    case "*":
-                        out.append(f"imul {line[0]}, {line[3]}")
-
-                    case "/":
-                        out.append(f"idiv {line[0]}, {line[3]}")
-
-        except IndexError:
-            pass
-
-        if line[0] == "func":
+            try:
+                if bss[0] != "section .bss":
+                    bss = ["section .bss"] + bss
+            except IndexError:
+                bss = ["section .bss"]
+        elif line[0] == "byte":
             try:
                 if line[2] == "=>":
-                    if scope == ["global"]:
-                        out.append(f"{line[1]}:")
-                        scope.append(f"{line[1]}")
-                        fncs.append(line[1])
-                    else:
-                        print(f"ScopeError: func {' '.join(line[1:])}, at line {i}: function definition outside 'global'")
-                        sys.exit(1)
+                    pre.append(f"{line[1]} db {" ".join(line[3:])}")
+                    vartype[line[1]] = "byte"
+                elif line[2] == "<=":
+                    bss.append(f"{line[1]}: resb {line[3]}")
+                    vartype[line[1]] = "byteptr"
+
             except IndexError:
-                print(f"SyntaxError: func {line[1]}, at line {i}: incomplete function definition")
-                sys.exit(1)
-        elif line[0] == "}":
-            scope.pop()
-            if out[-1] != "ret":
-                out.append("ret")
+                bss.append(f"{line[1]}: resb 1")
+                vartype[line[1]] = "byteptr"
 
-        try:
-            if line[1] == "<=":
-                if line[2].startswith("(ptr)"):
-                    out.append(f"lea r12, [{''.join(line[2][5:])}]")
-                else:
-                    out.append(f"mov r12, {line[2]}")
+        elif line[0] == "pipe":
+            try:
+                if line[2] == "=>":
+                    pre.append(f"{line[1]} EQU {' '.join(line[3:])}")
+                elif line[2] == "<=":
+                    pre.append(f"{line[3]} EQU {line[1]}")
+            except IndexError:
+                print(f"Error: At line {i}")
 
+        elif line[0] == "short":
+            try:
+                if line[2] == "=>":
+                    pre.append(f"{line[1]} dw {line[3]}")
+                    vartype[line[1]] = "short"
+
+            except IndexError:
+                pre.append(f"{line[1]}: resb 2")
+                vartype[line[1]] = "shortptr"
+
+        elif line[0] == "dword":
+            if line[2] == "<=":
+                bss.append(f"{line[1]}: resd {line[3]}")
+
+
+        elif line[0] == "asmdef":
+            pre.append(" ".join(line[1:]))
+
+        elif line[0] == "asm":
+            out.append(" ".join(line[1:]))
+
+        elif line[0] == "label":
+            pre.append(f"{line[1]}:")
+
+        elif line[0] == "return":
+            try:
+                out.append(f"mov rax, {line[1]}")
+            except IndexError:
+                pass
+            out.append("ret")
+
+        elif line[0] == "=>":
+            out.append(f"jmp {line[1]}")
+
+        else:
+            try:
+                if line[1] == "<=" and line[0] not in libs and line[0] not in fncs:
+                    out.append(f"mov {line[0]}, {line[2]}")
+                    i += 1
+                    continue
+
+                elif line[1] == "<":
+                    match line[2]:
+                        case "+":
+                            out.append(f"add {line[0]}, {line[3]}")
+
+                        case "-":
+                            out.append(f"sub {line[0]}, {line[3]}")
+
+                        case "*":
+                            out.append(f"imul {line[0]}, {line[3]}")
+
+                        case "/":
+                            out.append(f"idiv {line[0]}, {line[3]}")
+
+            except IndexError:
+                pass
+
+            if line[0] == "func":
                 try:
-                    if line[3] == "<=":
-                        if line[4].startswith("(ptr)"):
-                            out.append(f"lea r13d, [{''.join(line[4][5:])}]")
+                    if line[2] == "=>":
+                        if scope == ["global"]:
+                            out.append(f"{line[1]}:")
+                            scope.append(f"{line[1]}")
+                            fncs.append(line[1])
                         else:
-                            out.append(f"mov r13d, {line[4]}")
-
-                        try:
-                            if line[5] == "<=":
-                                if line[6].startswith("(ptr)"):
-                                    out.append(f"lea r14, [{''.join(line[6][5:])}]")
-                                else:
-                                    out.append(f"mov r14, {line[6]}")
-                        except IndexError:
-                            pass
-
+                            print(f"ScopeError: func {' '.join(line[1:])}, at line {i}: function definition outside 'global'")
+                            sys.exit(1)
                 except IndexError:
-                    pass
-                
-                if line[0] in fncs:
-                    out.append(f"call {line[0]}")
-                elif line[0] in libs:
-                    out = out + libs[line[0]]
-                else:
-                    print(f"NameError, {line[0]}, at line {i}: Undefined Function or Unimported Module")
+                    print(f"SyntaxError: func {line[1]}, at line {i}: incomplete function definition")
                     sys.exit(1)
+            elif line[0] == "}":
+                scope.pop()
+                if out[-1] != "ret":
+                    out.append("ret")
 
-        except IndexError:
-            if line[0] in libs:
-                out = out + libs[line[0]]
+            try:
+                if line[1] == "<=":
+                    if line[2].startswith("(ptr)"):
+                        out.append(f"lea r12, [{''.join(line[2][5:])}]")
+                    else:
+                        out.append(f"mov r12, {line[2]}")
 
-    i += 1
+                    try:
+                        if line[3] == "<=":
+                            if line[4].startswith("(ptr)"):
+                                out.append(f"lea r13d, [{''.join(line[4][5:])}]")
+                            else:
+                                out.append(f"mov r13d, {line[4]}")
+
+                            try:
+                                if line[5] == "<=":
+                                    if line[6].startswith("(ptr)"):
+                                        out.append(f"lea r14, [{''.join(line[6][5:])}]")
+                                    else:
+                                        out.append(f"mov r14, {line[6]}")
+                            except IndexError:
+                                pass
+
+                    except IndexError:
+                        pass
+                    
+                    if line[0] in fncs:
+                        out.append(f"call {line[0]}")
+                    elif line[0] in libs:
+                        out = out + libs[line[0]]
+                    else:
+                        print(f"NameError, {line[0]}, at line {i}: Undefined Function or Unimported Module")
+                        sys.exit(1)
+
+            except IndexError:
+                if line[0] in libs:
+                    out = out + libs[line[0]]
+
+        i += 1
+        currentline += 1
+    return pre, bss, out
 
 out = ["section .text", f"global {mainfn}"] + out
        
